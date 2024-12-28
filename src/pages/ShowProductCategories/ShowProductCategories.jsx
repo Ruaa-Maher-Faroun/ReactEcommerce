0ae330bf68205929034ec84b550b62ca622789ac
Ruaa-Maher-Faroun <Roaa.maher.faroun@gmail.com>
2024-12-28 23:02:23 +0200

    fixed

--- src/pages/ShowProductCategories/ShowProductCategories.jsx
+++ src/pages/ShowProductCategories/ShowProductCategories.jsx
@@ -4,7 +4,7 @@ import Products from '../../components/Products/Products';
 import CustomizedCategories from '../CustomizedCategories/CustomizedCategories';
 import useCategoryFilter from '../../CustomHooks/useCategoryFilter/useCategoryFilter';
 import Loader from '../../components/Loader/Loader';
-import UseFetch from '../../CustomHooks/useFetch/UseFetch.jsx';
+import UseFetch from '../../CustomHooks/useFetch/UseFetch';
 
 
 export default function ShowProductCategories() {
